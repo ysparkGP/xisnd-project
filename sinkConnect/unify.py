a99db173d6fdc45f871e02768a9efc474f9cf28b
ysparkGP <yspark@goldenplanet.co.kr>
2023-09-24 11:14:04 +0900

    멀티쓰레딩으로 인한 INSERT 시, Unique Key 위배 문제 해결(UPSERT)

--- sinkConnect/unify.py
+++ sinkConnect/unify.py
@@ -2,100 +2,165 @@ from sqlalchemy import MetaData, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.dialects.postgresql import insert
 import os
 from dotenv import load_dotenv
 from model import gprtm_models as gprtm
 import itertools
 from collections import Counter
+from functools import cmp_to_key
+import copy
 
+def make_unify_upsert_data(record):
+    upsert_data = {
+        'uuid' : record.uuid,
+        'seq' : record.seq,
+        'cust_nm' : record.cust_nm,
+        'cust_email' : record.cust_email,
+        'cust_phone' : record.cust_phone,
+        'cust_addr' : record.cust_addr,
+        'cust_addr_state' : record.cust_addr_state,
+        'cust_addr_city' : record.cust_addr_city,
+        'cust_birth_d' : record.cust_birth_d,
+        'cust_register_dt' : record.cust_register_dt,
+        'cust_modify_dt' : record.cust_modify_dt,
+        'cust_gender_tp' : record.cust_gender_tp,
+        'cust_marketing' : record.cust_marketing,
+        'register_dt' : record.register_dt,
+        'modify_dt' : record.modify_dt
+    }
 
+    return upsert_data
+
+# 수정날짜 기준 커스텀 Comparator
+def reverse_comparator(a,b):
+    if a.cust_modify_dt >= b.cust_modify_dt : return -1
+    elif a.cust_modify_dt < b.cust_modify_dt : return 1
+
+def comparator(a,b):
+    if a.cust_modify_dt <= b.cust_modify_dt : return -1
+    elif a.cust_modify_dt > b.cust_modify_dt : return 1
+    
+# Source 기준 순차 삭제
+def source_priority(li, sources):
+    result_li = []
+    for record in li:
+        for source in sources:
+            if record.channel_tp == source:
+                result_li.append(record)
+        if len(result_li) > 0 : break
+
+    return result_li
+
+# Date 정렬 및 순차 삭제
+def time_priority(li, order):
+    if order == 'new':
+        li.sort(key=cmp_to_key(reverse_comparator))
+    else :
+        li.sort(key=cmp_to_key(comparator))
+    
+    result_li = []
+    if li:
+        value = li[0].cust_modify_dt
+        for record in li:
+            if record.cust_modify_dt == li[0].cust_modify_dt:
+                result_li.append(record)
+            else : break
+    return result_li
+
+# Frequency 기준 정렬 및 순차 삭제
+def frequency_priority(li, column_name, condition_value):
+    column = getattr(gprtm.Union_customer, column_name)
+    frequency_dict = {}
+    # condition_value = 1 이면 많은 순, 0 이면 적은 순
+    cnt = 0 if condition_value else 999999
+    value = None
+    
+    # 내림차순
+    if condition_value == 1:
+        # 통합 기준 컬럼 값 빈도 사전 만들기
+        for record in li:
+            column_value = getattr(record, column_name)
+            frequency_dict[column_value] = frequency_dict.get(column_value,0) + 1
+            if cnt < frequency_dict[column_value]:
+                cnt = frequency_dict[column_value]
+                value = column_value
+
+    # 오름차순
+    else :
+        for record in li:
+            column_value = getattr(record, column_name)
+            frequency_dict[column_value] = frequency_dict.get(column_value,0) + 1
+            if cnt > frequency_dict[column_value]:
+                cnt = frequency_dict[column_value]
+                value = column_value
+
+    result_li = []
+    for record in li:
+        if getattr(record, column_name) == value:
+            result_li.append(record)
+    return result_li
+    
+
+# 통합 컬럼 값 우선순위 진행
 def decide_priority(session, last_unified_record, seq, uuid_param):
-    # 통합 룰 검색 후 'logic' 
-    query = ' select rule_json from gprtm.rule_manager; '
-    json = session.execute(query).all()[0][0]
-    new_unify = []
-    for logic in json['logic']:
-        # print(logic)
-        field = logic['field']
-        # 멤버 변수 동적으로 불러오기
-        value = getattr(gprtm.Union_customer, field)
-        conditions = logic['conditions']
-        unify_priorities = []
-        
-        isExit = False
-        for condition in conditions:
-            if isExit : break
-
-            condition_name = list(condition.keys())[0]
-            # step 1. 'Source'
-            # Source 단계가 첫 번째가 되어야 함.
-            # Source 단계에서 후보군들을 찾아 리스트 삽입
-            if condition_name == 'Source':
-                source_priorities = condition[condition_name]
-                # print(source_priorities)
-                for source_priority in source_priorities:
-                    # 건네받은 uuid를 조건으로, link와 union을 조인시켜 Union_customer 검색
-                    temp_list = [session.query(gprtm.Union_customer)\
-                                        .filter(
-                                            gprtm.Link_customer.uuid==uuid_param,
-                                            gprtm.Link_customer.channel_tp == gprtm.Union_customer.channel_tp,
-                                            gprtm.Link_customer.cust_seq == gprtm.Union_customer.cust_seq,
-                                            gprtm.Link_customer.channel_tp == source_priority,
-                                            # gprtm.Union_customer.cust_nm.isnot(None)
-                                            value.isnot(None)
-                                        #  f'gprtm.Union_customer.{field}.isnot(None)'
-                                        ).all()
-                                        # .join(
-                                        #     gprtm.Link_customer, 
-                                        #     gprtm.Union_customer.channel_tp == gprtm.Link_customer.channel_tp,
-                                        #     gprtm.Union_customer.cust_seq == gprtm.Link_customer.cust_seq
-                                        # )\
-                                ]
-                    # Source 우선순위대로 1차원 리스트 생성
-                    # 위 쿼리에서 나온 결과물이 없으면 넘어감
-                    if len(temp_list) >= 1:
-                        unify_priorities+=(temp_list)
-                        # Source 우선순위만 보고 끝나는 상황
-                        if len(temp_list) == 1:
-                            unify_priorities = list(itertools.chain(*unify_priorities)) # flatten
-                            isExit = True
-                            break 
-
-                # 통합 요소 없음
-                if len(unify_priorities) == 0:
-                    break
-        
-            # step 2. 'Date'
-            elif condition_name == 'Date':
-                # Union_customer 의 cust_modify_dt 내림차순 정렬
-                # Step1 을 거쳐 만들어진 unify_priorities(채널소스 우선순위대로 쌓인, 길이가 2이상인 2차원 배열) 을 정렬 및 2차원 -> 1차원
-                unify_priorities[0].sort(key=lambda object: object.cust_modify_dt, reverse=True)
-                unify_priorities = list(itertools.chain(*unify_priorities)) # flatten
-                if unify_priorities[0].cust_modify_dt != unify_priorities[1].cust_modify_dt:
-                    break
-
-            # step 3. 'Frequency'
-            elif condition_name == 'Frequency':
-                # 각 채널들 전부에서 카운트 하는건지, 채널을 기준으로 독립적이게 카운트 하는건지?
-                # 만약 우선순위 2 CX = [3,4,3], 우선순위 3 DI = [4,4] 라면 3번,4번 중 어느 것이 뽑혀야하는건지?
-                continue
-        
-
-        print(f'--------------------{field}---------------------')
-        print(unify_priorities)
-        # for unify_priority in unify_priorities:
-            # print(unify_priority)
-            # print(f'{unify_priority.channel_tp}, {unify_priority.cust_seq}, {unify_priority.cust_nm} ')
-            
-        value = unify_priorities[0] if len(unify_priorities) > 0 else None
-        setattr(last_unified_record, field, value)
-
-
-def unify_start(session, seq, task_dml_tp, asis_uuid, tobe_uuid):
-    # Insert
     try:
-        if task_dml_tp == 'INSERT':
+        query = ' select rule_json from gprtm.rule_manager; '
+        json = session.execute(query).all()[0][0]
+        new_unify = []
 
+        # 통합 컬럼 값을 정할 Union 레코드들 미리 뽑기
+        original_unify_priorities = session.query(gprtm.Union_customer)\
+                                            .filter(
+                                                gprtm.Link_customer.uuid==uuid_param,
+                                                gprtm.Link_customer.channel_tp==gprtm.Union_customer.channel_tp,
+                                                gprtm.Link_customer.cust_seq==gprtm.Union_customer.cust_seq
+                                            ).all()
+        for logic in json['logic']:
+            field = logic['field']
+            conditions = logic['conditions']
+            
+            # 쿼리 부하를 줄이기 위해 객체 깊은 복사를 썼지만 그렇게 빠른 방법은 아니기에 지켜봐야함
+            temp_unify_priorities = []
+            for original_unify_priority in original_unify_priorities:
+                temp_unify_priorities.append(copy.deepcopy(original_unify_priority))
+
+
+            # 통합 컬럼 값이 NULL이 아닌 값들만 취급
+            unify_priorities = []
+            for unify in temp_unify_priorities:
+                if getattr(unify, field) is not None:
+                    unify_priorities.append(unify)
+
+            for condition in conditions:
+                condition_name = list(condition.keys())[0]
+                if condition_name == 'Source':
+                    unify_priorities = source_priority(unify_priorities, condition[condition_name])
+                elif condition_name == 'Date':
+                    unify_priorities = time_priority(unify_priorities, condition[condition_name])
+                elif condition_name == 'Frequency':
+                    unify_priorities = frequency_priority(unify_priorities, field, condition[condition_name])
+                
+            # 통합할 컬럼 값이 존재하면 update
+            if len(unify_priorities) > 0 : 
+                setattr(last_unified_record, field, getattr(unify_priorities[0], field))
+            else : 
+                setattr(last_unified_record, field, None)
+    except Exception as e:
+        print(e)
+        raise Exception(e)
+
+
+def unify_start(session, seq, task_dml_tp, asis_uuid, tobe_uuid, effected_uuids, changed_effected_uuids):
+    try:
+        if effected_uuids != None and len(effected_uuids) > 0:
+            for effected_uuid in effected_uuids :
+                print('effected_uuid -> effected_uuid unify 제거 후 tobe_uuid unify reconciliation')
+                effected_unified_record = session.query(gprtm.Unify_customer).filter(gprtm.Unify_customer.uuid==effected_uuid).first()
+                if effected_unified_record:
+                    session.delete(effected_unified_record)
+
+        if task_dml_tp == 'INSERT':
             # Insert 시 unified_customer 테이블에 같은 uuid가 있는지 검색
             last_unified_record = session.query(gprtm.Unify_customer).filter(gprtm.Unify_customer.uuid==tobe_uuid).first()
             # Insert 시 새로운 통합 레코드가 생성되거나, 통합되어진 레코드에 끼거나
@@ -105,43 +170,115 @@ def unify_start(session, seq, task_dml_tp, asis_uuid, tobe_uuid):
             else :
                 # 새로운 레코드 Insert
                 last_unified_record = gprtm.Unify_customer(tobe_uuid)
-                decide_priority(session, last_unified_record, seq, tobe_uuid)
-                session.add(last_unified_record)
+                if last_unified_record:
+                    # 멀티쓰레딩으로 인한 동시성 문제때문에 INSERT -> UPSERT
+                    decide_priority(session, last_unified_record, seq, tobe_uuid)
+                    upsert_data = make_unify_upsert_data(last_unified_record)
+                    insert_stmt = insert(gprtm.Unify_customer).values(
+                        upsert_data
+                    )
+
+                    do_update_stmt = insert_stmt.on_conflict_do_update(
+                        constraint='unify_customer_un',
+                        set_=upsert_data
+                    )
+
+                    session.execute(do_update_stmt)
             
-        # Update   
+            
         elif task_dml_tp == 'UPDATE':
             
             # Update 시 asis_uuid 와 tobe_uuid 를 비교 후, 같으면 tobe_uuid에만 통합진행, 다르면 asis, tobe 둘 다 진행
             if asis_uuid == tobe_uuid: 
                 last_unified_record = session.query(gprtm.Unify_customer).filter(gprtm.Unify_customer.uuid==tobe_uuid).first()
-                decide_priority(session, last_unified_record, seq, tobe_uuid)
+                if last_unified_record:
+                    decide_priority(session, last_unified_record, seq, tobe_uuid)
             else:
                 # asis_uuid 와 tobe_uuid 가 다른 경우,
                 # asis_uuid 의 통합 고객이 새로 업데이트 되어지고, tobe_uuid 의 통합 고객도 새로 업데이트 되어져야함
+                # 단, asis_uuid는 없어졌는지, 남아있는지를 확인해야함
+                # tobe_uuid 를 가진 unify 가 새로 생성될 수 있음
+                asis_last_link_unified_record = session.query(gprtm.Unify_customer)\
+                                                .filter(gprtm.Unify_customer.uuid==asis_uuid,
+                                                        gprtm.Unify_customer.uuid==gprtm.Link_customer.uuid)\
+                                                .first()
                 tobe_last_unified_record = session.query(gprtm.Unify_customer).filter(gprtm.Unify_customer.uuid==tobe_uuid).first()
-                decide_priority(session, tobe_last_unified_record, seq, tobe_uuid)
-                asis_last_unified_record = session.query(gprtm.Unify_customer).filter(gprtm.Unify_customer.uuid==asis_uuid).first()
-                decide_priority(session, asis_last_unified_record, seq, tobe_uuid)
+
+                if asis_last_link_unified_record : 
+                    print('asis_uuid unify 남아있어서 업데이트 해야함')
+                    asis_last_unified_record = session.query(gprtm.Unify_customer).filter(gprtm.Unify_customer.uuid==asis_uuid).first()
+                    if asis_last_unified_record: 
+                        decide_priority(session, asis_last_unified_record, seq, asis_uuid)
+                
+                else : 
+                    print('asis_uuid unify 제거해야함')
+                    asis_last_unified_record = session.query(gprtm.Unify_customer).filter(gprtm.Unify_customer.uuid==asis_uuid).first()
+                    if asis_last_unified_record:
+                        session.delete(asis_last_unified_record)
+
+                if tobe_last_unified_record :
+                    print('tobe_uuid unify 남아있어서 업데이트 해야함')
+                    decide_priority(session, tobe_last_unified_record, seq, tobe_uuid)
+                        
+                else :
+                    print('tobe_uuid unify 없어서 새로 생성해야함')
+                    last_unified_record = gprtm.Unify_customer(tobe_uuid)
+                    if last_unified_record:
+                        decide_priority(session, last_unified_record, seq, tobe_uuid)
+                        # 멀티쓰레딩으로 인한 동시성 문제때문에 INSERT -> UPSERT
+                        upsert_data = make_unify_upsert_data(last_unified_record)
+                        insert_stmt = insert(gprtm.Unify_customer).values(
+                            upsert_data
+                        )
+
+                        do_update_stmt = insert_stmt.on_conflict_do_update(
+                            constraint='unify_customer_un',
+                            set_=upsert_data
+                        )
+
+                        session.execute(do_update_stmt)
 
 
-        # Delete
         elif task_dml_tp == 'DELETE':
             
             # Delete 시 asis_uuid로만 판단하고, 기존 통합 레코드 업데이트나 삭제되는 경우
             # Delete 되어진 고객이 link_customer 에서 남아있는지 검색
-            last_link_records = session.query(gprtm.Link_customer).filter(gprtm.Link_customer.uuid==asis_uuid).all()
+            last_link_record = session.query(gprtm.Link_customer).filter(gprtm.Link_customer.uuid==asis_uuid).first()
             unified_record = session.query(gprtm.Unify_customer).filter(gprtm.Unify_customer.uuid == asis_uuid).first()
-            if len(last_link_records) > 0:
+            # print(last_link_record)
+            if last_link_record :
                 # 남아있다면 통합 레코드 업데이트
-                decide_priority(session, unified_record, seq, tobe_uuid)
+                if unified_record:
+                    decide_priority(session, unified_record, seq, asis_uuid)
             else :
-                session.delete(unified_record)
+                # link에는 없지만 {unified_record.uuid}가 unify에 존재해서 삭제
+                if unified_record :
+                    session.delete(unified_record)
                 # 없으면 기존에 남아있던 통합 레코드 제거
+        
 
+        if changed_effected_uuids != None and len(changed_effected_uuids) > 0:
+            print('changed_effected_uuid -> asis_uuid가 바뀌었으니, asis_uuid_unify 제거 또는 변경(로직 이미 존재) 후 changed_uuid_unify insert, reconciliation')
+            for changed_effected_uuid in changed_effected_uuids:
+                changed_unified_record = gprtm.Unify_customer(changed_effected_uuid)
+                if changed_unified_record:
+                    decide_priority(session, changed_unified_record, seq, changed_effected_uuid)
+                    # 멀티쓰레딩으로 인한 동시성 문제때문에 INSERT -> UPSERT
+                    upsert_data = make_unify_upsert_data(changed_unified_record)
+                    insert_stmt = insert(gprtm.Unify_customer).values(
+                        upsert_data
+                    )
+
+                    do_update_stmt = insert_stmt.on_conflict_do_update(
+                        constraint='unify_customer_un',
+                        set_=upsert_data
+                    )
+                    session.execute(do_update_stmt)
+                    
+        
         session.commit()
-
         return True
     
     except Exception as e:
-        print(f'통합 과정 에러 발생 {e}')
+        print(f'task_record {seq} 통합 과정 에러 발생 {e}')
         return False
